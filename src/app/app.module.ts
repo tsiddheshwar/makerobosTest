4dede261a05bf11f5dc57621aae4fbb0d9650189
tsiddheshwar <tsiddheshwar@gmail.com>
2023-03-25 23:21:52 +0530

    added sample navbars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarsComponent } from './nav-bars/nav-bars.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/nav-bars/nav-bars.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bars',
+  templateUrl: './nav-bars.component.html',
+  styleUrls: ['./nav-bars.component.css']
+})
+export class NavBarsComponent {
+
+}
